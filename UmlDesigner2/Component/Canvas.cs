cd274a139f6ebf7b4195cd483ec6cf7b6fb7ede6
Robson10 <mucharobert4@wp.pl>
2017-02-15 14:19:04 +0100

    Add project files.

---
+++ UmlDesigner2/Component/Canvas.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UmlDesigner2.Component
+{
+    public partial class Canvas : UserControl
+    {
+        List<MyCanvasObject> CanvasObjects = new List<MyCanvasObject>();
+        private MyDictionary.Shape _ShapeToDraw = MyDictionary.Shape.Start;
+        public MyDictionary.Shape ShapeToDraw
+        {
+            get {return _ShapeToDraw; }
+            set
+            {
+                _ShapeToDraw = value;
+                if(value==MyDictionary.Shape.Nothing)
+                {
+                    Cursor = Cursors.Arrow;
+                }
+                else
+                {
+                    Cursor = Cursors.Cross;
+                }
+            }
+        } 
+
+
+        public Canvas()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            BackColor = Color.Blue;
+        }
+        public void ResizeAll( Size clientSize)
+        {
+            Size = clientSize;
+        }
+        //lewy utworzenie controlki, zaznaczenie, ppm menu kontekstowe
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (ShapeToDraw != MyDictionary.Shape.Nothing)
+                {
+                    CanvasObjects.Add(
+                        new MyCanvasObject(
+                            new Rectangle(e.Location.X-MyDictionary.defaultCanvasControlSize.Width/2,e.Location.Y- MyDictionary.defaultCanvasControlSize.Height/ 2, MyDictionary.defaultCanvasControlSize.Width, MyDictionary.defaultCanvasControlSize.Height), 
+                            ShapeToDraw));
+                    ShapeToDraw = MyDictionary.Shape.Nothing;
+                }
+                else
+                    for (int i = 0; i < CanvasObjects.Count; i++)
+                        if (CanvasObjects[i].IsContain(e.Location))
+                            CanvasObjects[i].IsSelected = true;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                for (int i = 0; i < CanvasObjects.Count; i++)
+                    if (CanvasObjects[i].IsContain(e.Location))
+                        MessageBox.Show("o to tu pojawić powinno sie menu kontekstowe dla ppm ale na chwile obecną poszło na piwo i do tej pory nie wrociło");
+            }
+            Invalidate();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode=System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            for (int i = 0; i < CanvasObjects.Count; i++)
+                CanvasObjects[i].Draw( e.Graphics);
+        }
+    }
+   
+
+    public class MyCanvasObject
+    {
+        public MyCanvasObject(Rectangle rect,MyDictionary.Shape shape)
+        {
+            Rect = rect;
+            Shape = shape;
+            Text = "";
+        }
+
+        public Rectangle Rect;//obszar dla figury - point , size
+
+        private MyDictionary.Shape _Shape;
+        public MyDictionary.Shape Shape//jaką kontrolke rysujemy
+        {
+            get { return _Shape; }
+            set
+            {
+                _Shape = value;
+                BackColor =MyDictionary.CanvasObjectBackColor(_Shape);
+                FontColor = MyDictionary.CanvasObjectFontColor(_Shape);
+                FontSize = MyDictionary.CanvasObjectFontSize(_Shape);
+
+            }
+        }
+        public SolidBrush BackColor;
+        public Color FontColor;
+        public int FontSize;
+        public string Text; //zawartosc tekstowa kontrolki
+        public bool IsSelected=false;//czy jest zaznaczona
+        public bool IsLocked = false;
+        public uint ID;
+        public int In, Out, Out2;
+        public bool IsContain(Point location)//http://stackoverflow.com/questions/34582234/how-to-detect-if-mouse-has-clicked-inside-of-a-certain-shape-in-c-sharp-on-winfo
+        {
+            var contains = false;
+            if (Shape==MyDictionary.Shape.Start)
+            { 
+                using (var gp = new System.Drawing.Drawing2D.GraphicsPath())
+                {
+                    gp.AddEllipse(Rect);
+                    contains = gp.IsVisible(location);
+                }
+            }
+            else if (Shape == MyDictionary.Shape.End)
+            {
+                using (var gp = new System.Drawing.Drawing2D.GraphicsPath())
+                {
+                    gp.AddEllipse(Rect);
+                    contains = gp.IsVisible(location);
+                }
+            }
+            else if (Shape == MyDictionary.Shape.Execution)
+            {
+                using (var gp = new System.Drawing.Drawing2D.GraphicsPath())
+                {
+                    gp.AddRectangle(Rect);
+                    contains = gp.IsVisible(location);
+                }
+            }
+            else if (Shape == MyDictionary.Shape.Input)
+            {
+                using (var gp = new System.Drawing.Drawing2D.GraphicsPath())
+                {
+                    gp.AddEllipse(Rect);
+                    contains = gp.IsVisible(location);
+                }
+            }
+            else if (Shape == MyDictionary.Shape.Decision)
+            {
+                using (var gp = new System.Drawing.Drawing2D.GraphicsPath())
+                {
+                    gp.AddEllipse(Rect);
+                    contains = gp.IsVisible(location);
+                }
+            }
+            return contains;
+        }
+
+
+        string Start = "start";
+        public  void Draw( Graphics g)
+        {
+            switch(Shape)
+            {
+                case (MyDictionary.Shape.Start):
+                    g.FillEllipse(BackColor, Rect);
+                    break;
+                case (MyDictionary.Shape.End):
+                    g.FillEllipse(BackColor, Rect);
+                    break;
+                case (MyDictionary.Shape.Input):
+                    g.FillEllipse(BackColor, Rect);
+                    break;
+                case (MyDictionary.Shape.Execution):
+                    g.FillEllipse(BackColor, Rect);
+                    break;
+                case (MyDictionary.Shape.Decision):
+                    g.FillEllipse(BackColor, Rect);
+                    break;
+                default:
+                    MessageBox.Show("nie znalazłem odpowiedniego kształtu i nwm co teraz mam narysować ;(");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ UmlDesigner2/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UmlDesigner2.Component;
+namespace UmlDesigner2
+{
+    public partial class Form1 : Form
+    {
+        MyToolStripContainer myToolStripContainer = new MyToolStripContainer();
+        Canvas Canvas = new Canvas();
+        public Form1()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+            AddStripMenuEvents();
+        }
+        private void AddStripMenuEvents()
+        {
+            myToolStripContainer.ContentPanel.Controls.Add(Canvas);
+            myToolStripContainer.NewFile_Click += MyToolStripContainer_NewFile_Click;
+            myToolStripContainer.OpenFile_Click += MyToolStripContainer_OpenFile_Click;
+            myToolStripContainer.SaveFile_Click += MyToolStripContainer_SaveFile_Click;
+            myToolStripContainer.SaveFileAs_Click += MyToolStripContainer_SaveFileAs_Click;
+            myToolStripContainer.Undo_Click += MyToolStripContainer_Undo_Click;
+            myToolStripContainer.Redo_Click += MyToolStripContainer_Redo_Click;
+            myToolStripContainer.Options_Click += MyToolStripContainer_Options_Click;
+            myToolStripContainer.LogIn_Click += MyToolStripContainer_LogIn_Click;
+            myToolStripContainer.OpenCloudFile_Click += MyToolStripContainer_OpenCloudFile_Click;
+            myToolStripContainer.Run_Click += MyToolStripContainer_Run_Click;
+            myToolStripContainer.Debug_Click += MyToolStripContainer_Debug_Click;
+
+            myToolStripContainer.Start_Click += MyToolStripContainer_Start_Click;
+            myToolStripContainer.End_Click += MyToolStripContainer_End_Click;
+            myToolStripContainer.Input_Click += MyToolStripContainer_Input_Click;
+            myToolStripContainer.Execution_Click += MyToolStripContainer_Execution_Click;
+            myToolStripContainer.Decision_Click += MyToolStripContainer_Decision_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myToolStripContainer.Size = ClientSize;
+            Controls.Add(myToolStripContainer);
+
+        }
+        #region Toolstrip Events
+        private void MyToolStripContainer_Decision_Click(object sender, EventArgs e)
+        {
+            Canvas.ShapeToDraw = MyDictionary.Shape.Decision;
+        }
+
+        private void MyToolStripContainer_Execution_Click(object sender, EventArgs e)
+        {
+            Canvas.ShapeToDraw = MyDictionary.Shape.Execution;
+        }
+
+        private void MyToolStripContainer_Input_Click(object sender, EventArgs e)
+        {
+            Canvas.ShapeToDraw = MyDictionary.Shape.Input;
+        }
+
+        private void MyToolStripContainer_End_Click(object sender, EventArgs e)
+        {
+            Canvas.ShapeToDraw = MyDictionary.Shape.End;
+        }
+
+        private void MyToolStripContainer_Start_Click(object sender, EventArgs e)
+        {
+            Canvas.ShapeToDraw = MyDictionary.Shape.Start;
+        }
+
+        private void MyToolStripContainer_Debug_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MyToolStripContainer_Run_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MyToolStripContainer_OpenCloudFile_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MyToolStripContainer_LogIn_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MyToolStripContainer_Options_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MyToolStripContainer_Redo_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MyToolStripContainer_Undo_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MyToolStripContainer_SaveFileAs_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MyToolStripContainer_SaveFile_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MyToolStripContainer_OpenFile_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MyToolStripContainer_NewFile_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            myToolStripContainer.MyResize(ClientSize);
+            Canvas.ResizeAll(ClientSize);
+        }
+
+    }
+}
